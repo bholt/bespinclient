717f2424eb691600104f8f7ead2f1ad9e4f24c89
Brandon Holt <holt.bg@gmail.com>
2010-09-27 16:24:37 -0500

    broke some stuff

--- plugins/supported/toolbar/items.js
+++ plugins/supported/toolbar/items.js
@@ -56,11 +56,19 @@ function OpenFileIndicator() {
     this.element = li;
 }
 
-function Save() {
-    var li = document.createElement('li');
-    li.innerHTML = "Save";
-    this.element = li;
+exports.Save = function Save() {
+    this.element = document.createElement('li');
+	this.init();
+};
+exports.Save.prototype = {
+	init: function() {
+		this.element.addEventListener('click', this._save);
+	},
+	_save: function() {
+		
+	}
 }
+
 
 exports.PositionIndicator = function PositionIndicator() {
     this.element = document.createElement('li');
@@ -71,16 +79,16 @@ exports.PositionIndicator.prototype = {
 	init: function() {
 		if(env.editor) {
 			env.editor.selectionChanged.add(this.updatePosition.bind(this));
+			this.updatePosition.call(this);
 		} else {
 			setTimeout(this.init.bind(this), 100);
 		}
 	},
 	updatePosition: function(range) {
 		range = range || env.editor.selection;
-		this.element.innerHTML = "Row " + (range.end.row + 1) + ", Column " + (range.end.col + 1);
+		this.element.innerHTML = "Row: " + (range.end.row + 1) + " Col: " + (range.end.col + 1);
 	}
 };
 
 exports.Logo = Logo;
 exports.OpenFileIndicator = OpenFileIndicator;
-exports.Save = Save;
